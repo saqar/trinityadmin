c34625a8399fc362d882c1ea5c17acad488f60d0
gsmith@middletonfire.org <gsmith@middletonfire.org@37492258-0088-11df-b136-7f7962f7bc17>
2011-01-16 20:21:06 +0000

    -=[ Revision 44 - 1/15/2011]=- - Forgot to update internal revision string, fixed

--- trunk/TrinityAdmin.lua
+++ trunk/TrinityAdmin.lua
@@ -34,7 +34,7 @@ gettingGOBinfo=0
 gettingGOBinfoinfo=0
 
 MAJOR_VERSION = "TrinityAdmin-3.3.5a"
-MINOR_VERSION = "$Revision: 042 $"
+MINOR_VERSION = "$Revision: 044 $"
 ROOT_PATH     = "Interface\\AddOns\\TrinityAdmin\\"
 local cont = ""
 if not AceLibrary then error(MAJOR_VERSION .. " requires AceLibrary") end
